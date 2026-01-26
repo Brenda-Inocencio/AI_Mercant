7a095b1c86536616eb0ac890d8f0356778190fcd
Brenda-Inocencio <binocencio@gaming.tech>
2026-01-26 18:20:13 +0100

    First commit

--- AI_Mercant/AI_Mercant/AI_Mercant.cpp
+++ AI_Mercant/AI_Mercant/AI_Mercant.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <optional>
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
 
+    // Load a sprite to display
+    const sf::Texture texture("block.png");
+    sf::Sprite sprite(texture);
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        while (const auto event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Draw the sprite
+        window.draw(sprite);
+
+        // Update the window
+        window.display();
+    }
+}
