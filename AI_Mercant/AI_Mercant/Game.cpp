b2fd48baae9c7b578c5548328d5be67c7611ff5a
Tatsuya <tbrouillard@gaming.tech>
2026-01-29 12:06:18 +0100

    first commit

---
+++ AI_Mercant/AI_Mercant/Game.cpp
@@ -0,0 +1,22 @@
+#include "Game.h"
+
+
+Game::Game() 
+	: m_phaseTime(0.f)
+{}
+
+void Game::update(float dt) {
+
+}
+
+void Game::updateMorning(float dt) {
+
+}
+
+void Game::updateDay(float dt) {
+
+}
+
+void Game::updateEvening(float dt) {
+
+}
