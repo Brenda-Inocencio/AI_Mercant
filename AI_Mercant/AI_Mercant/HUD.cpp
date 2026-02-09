685d2b056905912160414e8075f6b2917809c7c6
Brenda-Inocencio <binocencio@gaming.tech>
2026-02-09 17:27:31 +0100

    merge

--- AI_Mercant/AI_Mercant/HUD.cpp
+++ AI_Mercant/AI_Mercant/HUD.cpp
@@ -2,49 +2,53 @@
 
 #include <iostream>
 
-HUD::HUD() : day(0), time(0) {}
+HUD::HUD() : day("0"), time("0") {}
 
 void HUD::Day(int d) {
-	day = d;
+	day = std::to_string(d);
+	// Creation text
+	dayText.setPosition(sf::Vector2f(470, 5));
+	dayText.setFillColor(sf::Color::Black); // Couleur du texte
 }
 
 void HUD::Time(float t) {
-	time = t;
+	time = std::to_string(t);
+	// Creation text
+	timeText.setPosition(sf::Vector2f(480, 40));
+	timeText.setFillColor(sf::Color::Black); // Couleur du texte
 }
 
-void HUD::Render(sf::RenderWindow& window, const std::string& message, float x, float y) {
-	RenderText(window, message, x, y);
-	RenderText1(window, message, x, y);
+void HUD::Render(sf::RenderWindow& window, int d, float t) {
+	RenderText(window);
+	Day(t);
+	window.draw(dayText);
+	RenderText1(window);
+	Time(t);
+	window.draw(timeText);
 }
 
-void HUD::RenderText(sf::RenderWindow& window, const std::string& message, float x, float y) {
+void HUD::RenderText(sf::RenderWindow& window) {
 
-	if (!font_.openFromFile("arial.ttf")) {
+	if (!font.openFromFile("arial.ttf")) {
 		std::cerr << "Error can't find font";
 	}
 
 	// Creation text
-	text.setFont(font_);
-	text.setString(message);
-	text.setCharacterSize(30); // Taille en pixels
-	text.setPosition(sf::Vector2f(x -220, y -255));
+	text.setPosition(sf::Vector2f(400, 5));
 	text.setFillColor(sf::Color::Black); // Couleur du texte
 	text.setStyle(sf::Text::Bold); // Style
 	window.draw(text);
 }
 
 
-void HUD::RenderText1(sf::RenderWindow& window, const std::string& message, float x, float y) {
+void HUD::RenderText1(sf::RenderWindow& window) {
 
-	if (!font_.openFromFile("arial.ttf")) {
+	if (!font.openFromFile("arial.ttf")) {
 		std::cerr << "Error can't find font";
 	}
 
 	// Creation text
-	text1.setFont(font_);
-	text1.setString(message);
-	text1.setCharacterSize(30); // Taille en pixels
-	text1.setPosition(sf::Vector2f(x -220, y -215));
+	text1.setPosition(sf::Vector2f(400, 40));
 	text1.setFillColor(sf::Color::Black); // Couleur du texte
 	text1.setStyle(sf::Text::Bold); // Style
 	window.draw(text1);
--- AI_Mercant/AI_Mercant/HUD.h
+++ AI_Mercant/AI_Mercant/HUD.h
@@ -2,37 +2,36 @@
 #define HUD_H
 
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Image.hpp>
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/Text.hpp>
 #include <string>
 #include "GameDay.h"
 
 
-class HUD{
+class HUD {
 public:
 	HUD();
 
-	
+
 	// Update value
 	void Day(int d);
 	void Time(float t);
 
 
-	void Render(sf::RenderWindow& window, const std::string& message, float x, float y);
+	void Render(sf::RenderWindow& window, int d, float t);
 
-	~HUD();
+	~HUD() {};
 private:
-	sf::Font* font;
-	sf::Font font_;
-	sf::Text text = sf::Text(font_, "Day", 48);
-	sf::Text text1 = sf::Text(font_, "Time", 48);
+	sf::Font font;
+	std::string day;
+	std::string time;
+	sf::Text text = sf::Text(font, "Day : ", 24);
+	sf::Text text1 = sf::Text(font, "Time : ", 24);
+	sf::Text dayText = sf::Text(font, day, 24);
+	sf::Text timeText = sf::Text(font, time, 24);
 
-	int day;
-	float time;
-
-	void RenderText(sf::RenderWindow& window, const std::string& message, float x, float y);
-	void RenderText1(sf::RenderWindow& window, const std::string& message, float x, float y);
+	void RenderText(sf::RenderWindow& window);
+	void RenderText1(sf::RenderWindow& window);
 };
 
 #endif 
